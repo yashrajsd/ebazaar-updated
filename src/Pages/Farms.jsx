46121895a84775377c31b034d3605c83c4b85562
Mohammad Abdul Lateef <mohammad@Mohammads-MacBook-Air.local>
2023-02-02 07:58:13 +0530

    Farms page- products, category, search implemented

--- src/Pages/Farms.jsx
+++ src/Pages/Farms.jsx
@@ -1,26 +1,20 @@
 import React, { useState, useContext } from 'react'
-import FarmsNavbar from './Farms Components/FarmsNavbar'
 import './Farms.scss'
 import Product from './Farms Components/Product'
 import { toast } from 'react-toastify';
-
 import { useParams } from 'react-router'
 import { useEffect } from 'react';
 import { collection, doc, getDocs, setDoc, } from 'firebase/firestore';
 import { db } from '../firebase';
 import { onSnapshot } from 'firebase/firestore';
-import { async } from '@firebase/util';
-import catContext from '../State/catContext';
 
 
 const Farms = ({ user }) => {
-  const productCollectionRef = collection(db, 'products');
   const { id } = useParams()
   const [farm, setFarm] = useState([])
   const [products, setProducts] = useState([])
-  const context = useContext(catContext);
-  const { cat, changeCat } = context;
   const [categoryProduct, setCategoryProduct] = useState([])
+  const [searchParams, setSearchParams] = useState('')
 
   let list = [];
   useEffect(() => {
@@ -42,23 +36,64 @@ const Farms = ({ user }) => {
     }
   }, [])
 
-  // console.log(categoryProduct);
+  const handle1 = () => {
+    setCategory('vegetables')
+    setCategoryProduct(products.filter(product => product.category == 'Vegetables'))
+
+  }
+  const handle2 = () => {
+    setCategory('fruits')
+    setCategoryProduct(products.filter(product => product.category == 'Fruits'))
+  }
+  const handle3 = () => {
+    setCategory('poultry')
+    setCategoryProduct(products.filter(product => product.category == 'Poultry Items'))
+  }
+  const handle4 = () => {
+    setCategory('dairy')
+    setCategoryProduct(products.filter(product => product.category == 'Dairy Items'))
+  }
+  const handle5 = () => {
+    setCategory('')
+    setCategoryProduct(products)
+  }
+  
+  const onChange = (e) => {
+    const val = e.target.value
+    setSearchParams(val)
+    if(val!=''){
+      setCategory('')
+      setCategoryProduct(products.filter(product => ((product.title.toLowerCase().includes(searchParams))||(product.discription.toLowerCase().includes(searchParams)))))
+    }else{setCategoryProduct(products)}
+    console.log(categoryProduct);
+  }
 
 
   const [category, setCategory] = useState()
   return (
     <>
-      <div className='farms-container'>
-        <FarmsNavbar />
-        <div className='div-2'>
-          <div className="product-container">
-            {categoryProduct.map((product) => {
-              return (
-                <Product key={product.id} product={product} farm={farm} />
-              )
-            }
-            )}
-          </div>
+      <div className='farm-navbar container'>
+        <div className='farm-nav row'>
+          {/* <p>Categories</p> */}
+
+          <ul className='col-md-7 col-lg-6 col-sm-12'>
+            <li onClick={handle5} className={`${category === 'vegetables' && 'activeCategory'}`} >All</li>
+            <li onClick={handle1} className={`${category === 'vegetables' && 'activeCategory'}`} >Vegetables</li>
+            <li onClick={handle2} className={`${category === 'fruits' && 'activeCategory'}`} >Fruits</li>
+            <li onClick={handle3} className={`${category === 'poultry' && 'activeCategory'}`} >Poultry Items</li>
+            <li onClick={handle4} className={`${category === 'dairy' && 'activeCategory'}`}>Dairy Items</li>
+          </ul>
+          <input className='col-md-7 col-lg-5 col-sm-12' type="search" placeholder='Search' onChange={onChange}/>
+        </div>
+      </div>
+      <div className='div-2'>
+        <div className="product-container">
+          {categoryProduct.map((product) => {
+            return (
+              <Product key={product.id} product={product} farm={farm} />
+            )
+          }
+          )}
         </div>
       </div>
     </>
