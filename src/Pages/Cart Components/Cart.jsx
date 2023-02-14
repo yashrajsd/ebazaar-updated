8f24b05f2db6cbf8a336d92a6a31539fc661d55e
Yashraj Deshmukh <107096154+yashrajsd@users.noreply.github.com>
2023-02-14 22:25:00 +0530

    Update Cart.jsx

--- src/Pages/Cart Components/Cart.jsx
+++ src/Pages/Cart Components/Cart.jsx
@@ -1,113 +1,41 @@
 import React, { useState } from 'react'
-import CartCard from './CartCard';
-import { toast } from 'react-toastify';
 import { useEffect } from 'react';
-import { collection, doc, getDoc } from 'firebase/firestore';
-import { db } from '../../firebase';
-import { onSnapshot } from 'firebase/firestore';
-
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Cart = ({ user }) => {
-  const [products, setProducts] = useState([])
-  const [cartList, setCartList] = useState([])
-  let list = [];
-  let newlist = [];
-  let tlist=[];
-  useEffect(() => {
-    console.log('rendering')
-    const unsub = onSnapshot(
-      collection(db, 'users', user?.uid, 'cart'),
-      (snapshot) => {
-        snapshot.docs.forEach((doc) => {
-          newlist.push({ id: doc.id, ...doc.data() })
-        });
-        setCartList(newlist)
-      }, (error) => {
-        console.log(error)
-      }
-    )
+    const [cartProduct,setCartProduct] = useState([]);
+    const fetchData = async() => {
+      let array=[];
+      const docRef = collection(db,"users",user?.uid,"cart")
+      const response = await getDocs(docRef)
+      response.forEach((document)=>{
+        array.push(document.data())
+      })
+      setCartProduct(array);
+};
+    useEffect(() => {
+      console.log("rendering")
+      fetchData();
 
-    // console.log(cartList)
-    // const getCartProducts = () => {
-    //   console.log("getCartProducts")
-    //   newlist.map(cartProduct => {
-    //     console.log(cartProduct.id)
-    //     const docRef = doc(db, 'products', cartProduct.id);
+  }, [fetchData]);
 
-    //     getDoc(docRef)
-    //       .then((doc) => {
-    //         console.log(doc.data())
-    //         list.push({ id: doc.id, ...doc.data() })
-    //         setProducts(list)
-    //       })
-    //   });
-    // }
+  
 
-    return () => {
-      unsub();
-      // getCartProducts();
-    }
-  }, [])
-
-
-  const handleClick = async (prodID) => {
-    // :TODO 
-    toast.warning(`Removed from cart`)
-  }
-
-  const getCartProducts = () => {
-      console.log(cartList)
-      console.log("getCartProducts")
-      newlist.map(cartProduct => {
-        console.log(cartProduct.id)
-        const docRef = doc(db, 'products', cartProduct.id);
-
-        getDoc(docRef)
-          .then((doc) => {
-            console.log(doc.data())
-            list.push({ id: doc.id, ...doc.data() })
-            setProducts(list)
-          })
-      });
-
-      return (
-        <>
-          <div className='div-2'>
-            <div className="product-container">
-            <button onClick={handleRefresh} className="refresh">refresh</button>
-              {products.map((product) => {
-                return (
-                  <CartCard key={product.id} product={product} handleClick={handleClick} />
-                )
-              }
-              )}
-            </div>
-          </div>
-        </>
-      )
-
-    }
-
-  const handleRefresh = () => {
-    console.log('refreshing');
-    getCartProducts();
-
-  }
 
 
   return (
     <>
-      <div className='div-2'>
+      {/* <div className='div-2'>
         <div className="product-container">
-        <button onClick={handleRefresh} className="refresh">refresh</button>
-          {/* {products.map((product) => {
+          {products.map((product) => {
             return (
               <CartCard key={product.id} product={product} handleClick={handleClick} />
             )
           }
-          )} */}
+          )}
         </div>
-      </div>
+      </div> */}
     </>
   )
 }
